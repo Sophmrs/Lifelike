09cadeecd6d889a732147c71c1656e7804ab8187
Sophia Menkes Reis <sophiamenkesreis@gmail.com>
2017-10-17 19:14:53 -0200

    Added enzyme and canvas test

---
+++ src/components/Canvas/Canvas.test.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {mount} from 'enzyme';
+
+import {Canvas} from './Canvas';
+import {RenderSettings} from '../../Settings';
+
+//Avoid rng behavior for snapshots
+Math.random = jest.fn(() => .5);
+
+describe('Canvas', ()=>{
+  const drawSettings: RenderSettings = {
+    scale: 1,
+    pos: [0,0],
+    blur: 1
+  }
+
+  it('should render a canvas element', ()=>{
+    const canvas = mount(<Canvas cells={[]} neighborQty={[]} settings={drawSettings}/>);
+    expect(canvas.childAt(0).is('canvas')).toBe(true);
+  });
+
+  it('should render correctly', ()=>{
+    const canvas = mount(<Canvas cells={[]} neighborQty={[]} settings={drawSettings}/>);
+    expect(canvas).toMatchSnapshot();
+  });
+});
